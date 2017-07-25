49899b07335a699323b9b8da86e60bb7094f5f6f
Matthias Kretschmann <m@kretschmann.io>
2017-07-25 17:53:58 +0200

    hero & general branding tinkering

---
+++ _src/_assets/scripts/9984.js
@@ -0,0 +1,234 @@
+const draw = SVG('drawing')
+
+const pattern = draw.pattern(20, 20, function(add) {
+    add.rect(20, 20).move(5, 5).fill('#000').opacity(.2)
+})
+
+const rect = draw.rect('100%', '100%')
+rect.fill(pattern)
+
+const nineNineEightFour = function() {
+
+    const nineGroup = draw.group()
+
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 30)
+    nineGroup.rect(15, 15).fill('#ccc').move(70, 30).opacity(.8)
+    nineGroup.rect(15, 15).fill('#ccc').move(90, 30)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 30)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(30, 50)
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 50).opacity(.5)
+    nineGroup.rect(15, 15).fill('#ccc').move(70, 50)
+    nineGroup.rect(15, 15).fill('#ccc').move(90, 50)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 50).opacity(.8)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 50)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(30, 70)
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 70)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 70).opacity(.5)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 70)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(30, 90)
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 90)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 90).opacity(.5)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 90)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(30, 110)
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 110)
+    nineGroup.rect(15, 15).fill('#ccc').move(70, 110)
+    nineGroup.rect(15, 15).fill('#ccc').move(90, 110).opacity(.5)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 110)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 110).opacity(.8)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 130)
+    nineGroup.rect(15, 15).fill('#ccc').move(70, 130)
+    nineGroup.rect(15, 15).fill('#ccc').move(90, 130).opacity(.5)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 130)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 130)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 150).opacity(.8)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 150)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 170)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 170)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 190)
+    nineGroup.rect(15, 15).fill('#ccc').move(70, 190)
+    nineGroup.rect(15, 15).fill('#ccc').move(90, 190).opacity(.5)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 190)
+    nineGroup.rect(15, 15).fill('#ccc').move(130, 190)
+
+    nineGroup.rect(15, 15).fill('#ccc').move(50, 210)
+    nineGroup.rect(15, 15).fill('#ccc').move(70, 210)
+    nineGroup.rect(15, 15).fill('#ccc').move(90, 210).opacity(.8)
+    nineGroup.rect(15, 15).fill('#ccc').move(110, 210)
+
+    const nine2Group = draw.group()
+
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 30)
+    nine2Group.rect(15, 15).fill('#ccc').move(210, 30).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(230, 30)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 30)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(170, 50).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 50)
+    nine2Group.rect(15, 15).fill('#ccc').move(210, 50).opacity(.8)
+    nine2Group.rect(15, 15).fill('#ccc').move(230, 50)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 50).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 50)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(170, 70)
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 70)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 70).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 70)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(170, 90)
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 90).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 90)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 90)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(170, 110)
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 110)
+    nine2Group.rect(15, 15).fill('#ccc').move(210, 110)
+    nine2Group.rect(15, 15).fill('#ccc').move(230, 110).opacity(.8)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 110)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 110)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 130)
+    nine2Group.rect(15, 15).fill('#ccc').move(210, 130)
+    nine2Group.rect(15, 15).fill('#ccc').move(230, 130).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 130)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 130)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 150)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 150)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 170).opacity(.8)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 170)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 190)
+    nine2Group.rect(15, 15).fill('#ccc').move(210, 190)
+    nine2Group.rect(15, 15).fill('#ccc').move(230, 190).opacity(.5)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 190)
+    nine2Group.rect(15, 15).fill('#ccc').move(270, 190)
+
+    nine2Group.rect(15, 15).fill('#ccc').move(190, 210)
+    nine2Group.rect(15, 15).fill('#ccc').move(210, 210)
+    nine2Group.rect(15, 15).fill('#ccc').move(230, 210)
+    nine2Group.rect(15, 15).fill('#ccc').move(250, 210)
+
+    const eightGroup = draw.group()
+
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 30)
+    eightGroup.rect(15, 15).fill('#ccc').move(350, 30).opacity(.5)
+    eightGroup.rect(15, 15).fill('#ccc').move(370, 30)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 30)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(310, 50)
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 50).opacity(.8)
+    eightGroup.rect(15, 15).fill('#ccc').move(350, 50)
+    eightGroup.rect(15, 15).fill('#ccc').move(370, 50).opacity(.5)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 50)
+    eightGroup.rect(15, 15).fill('#ccc').move(410, 50)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(310, 70)
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 70)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 70).opacity(.5)
+    eightGroup.rect(15, 15).fill('#ccc').move(410, 70)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(310, 90)
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 90)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 90)
+    eightGroup.rect(15, 15).fill('#ccc').move(410, 90)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 110)
+    eightGroup.rect(15, 15).fill('#ccc').move(350, 110)
+    eightGroup.rect(15, 15).fill('#ccc').move(370, 110).opacity(.8)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 110)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 130)
+    eightGroup.rect(15, 15).fill('#ccc').move(350, 130).opacity(.5)
+    eightGroup.rect(15, 15).fill('#ccc').move(370, 130)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 130)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(310, 150)
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 150)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 150)
+    eightGroup.rect(15, 15).fill('#ccc').move(410, 150)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(310, 170)
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 170)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 170).opacity(.5)
+    eightGroup.rect(15, 15).fill('#ccc').move(410, 170)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(310, 190)
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 190)
+    eightGroup.rect(15, 15).fill('#ccc').move(350, 190)
+    eightGroup.rect(15, 15).fill('#ccc').move(370, 190)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 190)
+    eightGroup.rect(15, 15).fill('#ccc').move(410, 190)
+
+    eightGroup.rect(15, 15).fill('#ccc').move(330, 210)
+    eightGroup.rect(15, 15).fill('#ccc').move(350, 210)
+    eightGroup.rect(15, 15).fill('#ccc').move(370, 210)
+    eightGroup.rect(15, 15).fill('#ccc').move(390, 210)
+
+    const fourGroup = draw.group()
+
+    fourGroup.rect(15, 15).fill('#ccc').move(450, 30)
+    fourGroup.rect(15, 15).fill('#ccc').move(470, 30)
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 30)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 30)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(450, 50).opacity(.5)
+    fourGroup.rect(15, 15).fill('#ccc').move(470, 50)
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 50)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 50)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(450, 70)
+    fourGroup.rect(15, 15).fill('#ccc').move(470, 70)
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 70)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 70)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(450, 90).opacity(.5)
+    fourGroup.rect(15, 15).fill('#ccc').move(470, 90)
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 90)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 90)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(450, 110)
+    fourGroup.rect(15, 15).fill('#ccc').move(470, 110)
+    fourGroup.rect(15, 15).fill('#ccc').move(490, 110)
+    fourGroup.rect(15, 15).fill('#ccc').move(510, 110)
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 110)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 110)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(450, 130)
+    fourGroup.rect(15, 15).fill('#ccc').move(470, 130)
+    fourGroup.rect(15, 15).fill('#ccc').move(490, 130).opacity(.5)
+    fourGroup.rect(15, 15).fill('#ccc').move(510, 130)
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 130)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 130)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 150)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 150)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 170)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 170)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 190)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 190).opacity(.5)
+
+    fourGroup.rect(15, 15).fill('#ccc').move(530, 210)
+    fourGroup.rect(15, 15).fill('#ccc').move(550, 210)
+
+    const nineNineEightFourGroup = draw.group()
+
+    nineNineEightFourGroup.add(nineGroup).add(nine2Group).add(eightGroup).add(fourGroup).addClass('nine-nine-eight-four')
+
+    nineNineEightFourGroup.transform({ x: -5, y: -5 }).opacity(.25)
+}
+nineNineEightFour()
+
+// const rect2 = draw.rect(12, 12).fill('#f09')
+//
+// rect2.attr('y', 3).attr('x', '10%').animate().attr('x', '50%')
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -215,6 +215,13 @@ export const images = () => src(SRC + '_assets/images/**/*')
 
 
 //
+// Copy Fonts
+//
+export const fonts = () => src(SRC + '_assets/fonts/**/*')
+    .pipe(dest(DIST + 'assets/fonts/'))
+
+
+//
 // Revision static assets
 //
 export const rev = (done) => {
@@ -310,7 +317,7 @@ const deployBanner = (done) => {
 // `gulp build` is the development build
 // `gulp build --production` is the production build
 //
-export const build = series(buildBanner, clean, jekyll, parallel(html, css, js, images, svg), rev, revReplace, criticalCss)
+export const build = series(buildBanner, clean, jekyll, parallel(html, css, js, images, svg, fonts), rev, revReplace, criticalCss)
 
 //
 // Build site, run server, and watch for file changes
